89869ce8e9e00f7ad22da1f723fa7d2922d6c3df
alsongarbuja <magar33alson@gmail.com>
2026-06-20 23:44:47 -0500

    docs: remove old docusaurus documentation with fumadocs package

--- apps/docs/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  content: [
-    "./docs/**/*.{md,mdx}",
-    "./src/**/**/*.{js,ts,jsx,tsx,mdx}",
-    "./docusaurus.config.ts",
-    "./node_modules/@thanka-digital/beej-component/dist/*.{js,ts,jsx,tsx,mdx}",
-  ],
-};
